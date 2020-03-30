1f93305c7e3e4a3c206d72808821f1a32e4e23c6
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-03-30 10:51:28 -0300

    Update middleware js

--- middlewares/secretMiddleware.js
+++ middlewares/secretMiddleware.js
@@ -14,3 +14,20 @@ const secretMd = {
 }
 
 module.exports = secretMd
+
+//segunda opção
+// module.exports = {
+//     secretToReq: ( secret, options ) => {
+
+//         if ( options.upper ) {
+//             secret = secret.toUpperCase()
+//         }
+
+//         return ( req, res, next ) => {
+//             console.log( 'inside middleware' )
+//             req.secret = secret
+//             next()
+//         }
+//     }
+// }
+
